5672601e9a260f8034de0e75334980ae0f20babc
Aqua Tech <78642236+AquaTech101@users.noreply.github.com>
2023-05-31 13:11:11 +0700

    Login system Commit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,9 +21,18 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;1,700&display=swap" rel="stylesheet">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
 </head>
 
 <body>
+    
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Selamat Datang.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
 
     <span class="form-outer-title">
         Pengisian Form Surat KUA Online
